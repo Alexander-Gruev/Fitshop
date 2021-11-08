6c4ff4675017e128aba837ef2e0ec834c3391f6a
Alexander Gruev <alexandergruev2000@gmail.com>
2021-11-08 12:57:21 +0200

    Added init methods for Users and Roles

--- src/main/java/com/example/fitshop/init/ConsoleRunner.java
+++ src/main/java/com/example/fitshop/init/ConsoleRunner.java
@@ -1,6 +1,7 @@
 package com.example.fitshop.init;
 
 import com.example.fitshop.service.ProductService;
+import com.example.fitshop.service.UserService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -8,13 +9,16 @@ import org.springframework.stereotype.Component;
 public class ConsoleRunner implements CommandLineRunner {
 
     private final ProductService productService;
+    private final UserService userService;
 
-    public ConsoleRunner(ProductService productService) {
+    public ConsoleRunner(ProductService productService, UserService userService) {
         this.productService = productService;
+        this.userService = userService;
     }
 
     @Override
     public void run(String... args) throws Exception {
         this.productService.initProducts();
+        this.userService.initUsersAndRoles();
     }
 }
--- src/main/java/com/example/fitshop/web/UserLoginController.java
+++ src/main/java/com/example/fitshop/web/UserLoginController.java
@@ -8,7 +8,7 @@ public class UserLoginController {
 
     @GetMapping("/users/login")
     public String login() {
-
         return "auth-login";
     }
+    
 }
