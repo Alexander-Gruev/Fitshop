13d254e5765741fef9cc7ba6b336df5ab5b6b5b7
Alexander Gruev <alexandergruev2000@gmail.com>
2021-11-17 20:54:14 +0200

    added Interceptor for stats about own profile views

--- src/main/java/com/example/fitshop/config/WebConfiguration.java
+++ src/main/java/com/example/fitshop/config/WebConfiguration.java
@@ -1,5 +1,6 @@
 package com.example.fitshop.config;
 
+import com.example.fitshop.web.interceptor.OwnProfileViewsInterceptor;
 import com.example.fitshop.web.interceptor.RequestsStatsInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -9,13 +10,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfiguration implements WebMvcConfigurer {
 
     private final RequestsStatsInterceptor requestsStatsInterceptor;
+    private final OwnProfileViewsInterceptor ownProfileViewsInterceptor;
 
-    public WebConfiguration(RequestsStatsInterceptor requestsStatsInterceptor) {
+    public WebConfiguration(RequestsStatsInterceptor requestsStatsInterceptor, OwnProfileViewsInterceptor ownProfileViewsInterceptor) {
         this.requestsStatsInterceptor = requestsStatsInterceptor;
+        this.ownProfileViewsInterceptor = ownProfileViewsInterceptor;
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(this.requestsStatsInterceptor);
+        registry.addInterceptor(this.ownProfileViewsInterceptor);
     }
 }
--- src/main/java/com/example/fitshop/web/StatsController.java
+++ src/main/java/com/example/fitshop/web/StatsController.java
@@ -1,5 +1,6 @@
 package com.example.fitshop.web;
 
+import com.example.fitshop.service.OwnProfileViewsService;
 import com.example.fitshop.service.RequestsStatsService;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
@@ -12,9 +13,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class StatsController {
 
     private final RequestsStatsService requestsStatsService;
+    private final OwnProfileViewsService ownProfileViewsService;
 
-    public StatsController(RequestsStatsService requestsStatsService) {
+    public StatsController(RequestsStatsService requestsStatsService, OwnProfileViewsService ownProfileViewsService) {
         this.requestsStatsService = requestsStatsService;
+        this.ownProfileViewsService = ownProfileViewsService;
     }
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
@@ -22,5 +25,12 @@ public class StatsController {
     public String requestsStats(Model model) {
         model.addAttribute("stats", requestsStatsService.getStats());
         return "requests-stats";
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("/ownProfileViews")
+    public String ownProfileViewsStats(Model model) {
+        model.addAttribute("views", this.ownProfileViewsService.getViews());
+        return "own-profile-views-stats";
     }
 }
