8c8805231316e9029f98f3b57cb6e84ebcf22441
Alexander Gruev <alexandergruev2000@gmail.com>
2021-12-13 11:28:20 +0200

    improved validation and messages for username and email

--- src/main/java/com/example/fitshop/repository/UserRepository.java
+++ src/main/java/com/example/fitshop/repository/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.Optional;
 
 @Repository
@@ -12,4 +13,5 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
     Optional<UserEntity> findByUsername(String username);
 
+    Optional<UserEntity> findByEmail(String email);
 }
