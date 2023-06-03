1822f5a9d4feae909473d4c0e5f9d896271a143d
Alexander Gruev <alexandergruev2000@gmail.com>
2023-06-03 13:18:39 +0300

    Add hide orders functionality to ordersScript.js

--- src/main/resources/static/js/ordersScript.js
+++ src/main/resources/static/js/ordersScript.js
@@ -1,16 +1,25 @@
-$('#showOrders').click(() => {
-    loadOrders()
+let btn = $('#showOrders');
+
+btn.click(() => {
+    handleOrdersClick(event)
 });
 
-function loadOrders() {
-    $("#orders-container").empty();
-    fetch("http://localhost:8080/orders/user").
-    then(response => response.json()).
-    then(json => json.forEach(o => {
+function handleOrdersClick(event) {
+    let btnText = event.target.textContent;
+
+    if(btnText === 'Show my orders') {
+        fetch("http://localhost:8080/orders/user").
+        then(response => response.json()).
+        then(json => json.forEach(o => {
         let liElement = document.createElement("li");
         liElement.textContent = `${o.productName} by ${o.productBrandName}
                                 for address: ${o.address}. Order created at ${o.created}`;
          $("#orders-container").append(liElement);
-    }))
-}
+        }));
+        btn.text('Hide my orders');
+    } else if (btnText === 'Hide my orders') {
+        $("#orders-container").empty();
+        btn.text('Show my orders');
+    }
 
+}
